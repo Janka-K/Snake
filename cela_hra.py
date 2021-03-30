85ffd78a508ba5cbcaafbc99df9e5407b014265c
Janka K <janakorycanova@gmail.com>
2021-03-30 22:09:41 +0200

    Uprava kodu ve skriptu cela_hra.py

--- cela_hra.py
+++ cela_hra.py
@@ -7,11 +7,19 @@ from nakresli_mapu import nakresli_mapu
 
 souradnice = [(0, 0), (1, 0), (2, 0)]
 
+soucet_tahu = 0
+pocet_tahu = 30
+ovoce = [(2,3)]
+
 
 while True:
-  nakresli_mapu(souradnice)
-  svetova_strana = input(f"Zadej svetovou stranu ve forme s=sever,j=jih,v=vychod,z=zapad: ")
-  pohyb(souradnice,svetova_strana)
+    soucet_tahu +=1
+    nakresli_mapu(souradnice,soucet_tahu,ovoce,pocet_tahu)
+    svetova_strana = input(f"Zadej svetovou stranu ve forme s=sever,j=jih,v=vychod,z=zapad: ")
+    pohyb(souradnice,svetova_strana)
+    if soucet_tahu == pocet_tahu:
+        soucet_tahu = 0
+
 
 
 
